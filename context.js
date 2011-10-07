c0307d303354fc93be1125ab4a307391415e3cce
Carter Cole <carter.cole@gmail.com>
2011-10-07 08:18:36 -0500

    first version

---
+++ context.js
@@ -0,0 +1,7 @@
+
+
+$(function(){
+
+$("#speakers").html('<h3>Your Speakers</h3><div style="width: 100px; float: left; margin: 0 10px 0 0;"><img style="margin-bottom: 10px;" src="/site_media/media/files/speaker/cartercole_100x100.jpg" title="Carter Cole"></div><p><strong><a href="/speakers/carter-cole/">Carter Cole</a></strong></p><div class="speaker-meta"><p><strong>BrandExtract</strong><br><em>Application Developer / SEO Analyst</em></p><small>Track: <a href="/Create"><strong>Create</strong> Speaker</a></small></div></section>');
+
+})
